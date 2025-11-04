f9bfc804377faa2d94017b21f38c36ec09c4a04e
flori-taishi <frnetotea@gmail.com>
2025-11-04 08:36:32 +0200

    Update supravietuitor.cpp

--- supravietuitor.cpp
+++ supravietuitor.cpp
@@ -1,7 +1,7 @@
 #include "Supravietuitor.h"
 #include <iostream>
 
-// Constructor de initializare
+
 Supravietuitor::Supravietuitor(const std::string& nume, int viata, int energie, 
                                int x, int y, bool cristal)
     : jucator(nume, viata, energie, x, y), areCristal(cristal), 
@@ -9,21 +9,20 @@ Supravietuitor::Supravietuitor(const std::string& nume, int viata, int energie,
     initializeazaAbilitatiSpeciale();
 }
 
-// Functie privata pentru abilitati speciale
+
 void Supravietuitor::initializeazaAbilitatiSpeciale() {
-    // Supravietuitorii au abilitati defensive/luminoase
-    jucator.getAbilitati().clear(); // Sterge abilitatile standard
-    // Adauga abilitati specifice supravietuitorilor
+ 
+    jucator.getAbilitati().clear(); 
 }
 
-// Activeaza cristal - FUNCTIE COMPLEXA
+
 bool Supravietuitor::activeazaCristal() {
     if (jucator.getEnergie() >= 20 && !areCristal) {
         areCristal = true;
         puncteLumina += 15;
         cristaleActivate++;
         
-        // Bonus la viata pentru activare cristal
+   
         jucator.setViata(jucator.getViata() + 10);
         jucator.setEnergie(jucator.getEnergie() - 20);
         
@@ -34,7 +33,7 @@ bool Supravietuitor::activeazaCristal() {
     return false;
 }
 
-// Repara cristal
+
 void Supravietuitor::reparaCristal() {
     if (areCristal && jucator.getEnergie() >= 10) {
         puncteLumina += 5;
@@ -43,19 +42,19 @@ void Supravietuitor::reparaCristal() {
     }
 }
 
-// Calculeaza bonus lumina - FUNCTIE COMPLEXA
+
 int Supravietuitor::calculeazaBonusLumina() const {
     int bonus = puncteLumina;
     
-    // Bonus pentru cristale activate
+
     bonus += cristaleActivate * 5;
     
-    // Bonus pentru viata mare
+ 
     if (jucator.getViata() > 80) {
         bonus += 10;
     }
     
-    // Bonus pentru energie mare
+  
     if (jucator.getEnergie() > 40) {
         bonus += 5;
     }
@@ -63,7 +62,7 @@ int Supravietuitor::calculeazaBonusLumina() const {
     return bonus;
 }
 
-// Operator <<
+
 std::ostream& operator<<(std::ostream& os, const Supravietuitor& supravietuitor) {
     os << "=== SUPRAVIETUITOR ===\n"
        << supravietuitor.jucator
