de9db6be7d43146c012f01541cea7932d6b55092
flori-taishi <frnetotea@gmail.com>
2025-12-23 07:10:55 +0200

    Update player.h

--- player.h
+++ player.h
@@ -3,54 +3,45 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
+#include "Entitate.h"
 #include "Abilitate.h"
 
-class Jucator {
-private:
-    std::string nume;
+// Jucator moștenește acum din Entitate
+class Jucator : public Entitate {
+protected: // protected pentru ca Om si Creatura sa le poata accesa
     int viata;
     int energie;
-    int pozitieX, pozitieY;
     std::vector<Abilitate> abilitati;
 
-    // Functie privata
     void initializeazaAbilitatiStandard();
 
 public:
-    // Constructor de initializare
+    // Constructorul pasează numele și coordonatele către clasa de bază Entitate
     Jucator(const std::string& nume = "", int viata = 100, int energie = 50, 
             int x = 0, int y = 0);
     
-    // Constructor de copiere
     Jucator(const Jucator& other);
-    
-    // Operator= de copiere
     Jucator& operator=(const Jucator& other);
     
-    // Destructor
-    ~Jucator();
+    // Destructor virtual pentru polimorfism
+    virtual ~Jucator();
 
-    // Getters const
-    std::string getNume() const { return nume; }
+    // Getters
     int getViata() const { return viata; }
     int getEnergie() const { return energie; }
-    int getPozitieX() const { return pozitieX; }
-    int getPozitieY() const { return pozitieY; }
     const std::vector<Abilitate>& getAbilitati() const { return abilitati; }
 
     // Setters
-    void setPozitie(int x, int y);
     void setViata(int viata) { this->viata = viata; }
     void setEnergie(int energie) { this->energie = energie; }
 
-    // Functii membre netriviale
+    // Metode de logică
     bool esteInViata() const;
     void deplasare(int deltaX, int deltaY);
-    bool poateFolosiAbilitate(int indexAbilitate) const;
-    void folosesteAbilitate(int indexAbilitate);
     
-    // Operator <<
-    friend std::ostream& operator<<(std::ostream& os, const Jucator& jucator);
+    // Metodele virtuale din Entitate (le lăsăm virtuale pure sau le implementăm parțial)
+    virtual void interactioneaza() override = 0; 
 };
 
 #endif
